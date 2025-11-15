125805ece2de7461796bd7930903b406abac98b9
Amoako Domson Wahab <143537125+amoakodomson25@users.noreply.github.com>
2025-11-15 22:06:38 +0000

    commit

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,20 @@
+import { UserCardProps } from "@/interfaces";
+
+const UserCard: React.FC<UserCardProps> = ({ user }) => {
+    return (
+      <div className="p-4 border rounded-lg">
+        <h2 className="text-xl font-semibold">{user.name}</h2>
+        <p>@{user.username}</p>
+        <p>Email: {user.email}</p>
+  
+        <p className="mt-2 font-medium">Address:</p>
+        <p>{user.address.street}, {user.address.city}</p>
+  
+        <p className="mt-2 font-medium">Company:</p>
+        <p>{user.company.name}</p>
+      </div>
+    );
+  };
+  
+  export default UserCard;
+  
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,10 +1,29 @@
+import UserCard from "@/components/common/UserCard";
+import { UserCardProps } from "@/interfaces";
 
+interface UsersPageProps {
+  posts: UserCardProps[];
+}
 
-const Index: React.FC = () => {
-    return (
-      <div className="">
-      </div>
-    )
-  }
-  
-   export default Index;
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-6">
+      {posts.map((user) => (
+        <UserCard key={user.user.id} {...user} />
+      ))}
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Users;
