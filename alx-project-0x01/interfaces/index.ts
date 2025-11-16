13470a8b7724b4c5dbab3e5a1dfd3ecec2898c04
Amoako Domson Wahab <143537125+amoakodomson25@users.noreply.github.com>
2025-11-16 05:18:54 +0000

    U

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -6,27 +6,37 @@ export interface PostProps {
 }
 export interface UserProps {
    user: {
-     id: number;
-     name: string;
-     username: string;
-     email: string;
-     address: {
-       street: string;
-       suite: string;
-       city: string;
-       zipcode: string;
-       geo: {
-         lat: string;
-         lng: string;
-       };
-     };
-     phone: string;
-     website: string;
-     company: {
-       name: string;
-       catchPhrase: string;
-       bs: string;
-     };
+      id: number;
+      name: string;
+      username: string;
+      email: string;
+      address: {
+         street: string;
+         suite: string;
+         city: string;
+         zipcode: string;
+         geo: {
+            lat: string;
+            lng: string;
+         };
+      };
+      phone: string;
+      website: string;
+      company: {
+         name: string;
+         catchPhrase: string;
+         bs: string;
+      };
    };
- }
- 
+}
+export interface PostData {
+   userId: number;
+   id?: number;
+   title: string;
+   body: string;
+}
+
+export interface PostModalProps {
+   onClose: () => void;
+   onSubmit: (post: PostData) => void;
+}
